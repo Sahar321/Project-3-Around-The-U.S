f8e046623b95cb7bea79e318f5ccdb2b7e7c48d2
Sahar321 <Saharm123456@gmail.com>
2021-12-23 22:56:42 +0200

    Project 7

---
+++ Project 7/js/ConfigData.js
@@ -0,0 +1,7 @@
+export const formConfigIndex = {
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__btn-submit",
+  inactiveButtonClass: "popup__btn-submit_inactive",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible"
+};
