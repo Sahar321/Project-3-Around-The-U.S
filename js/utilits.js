490e112a2e2e5d68aa0eb9293606a2821cd2eee2
Sahar321 <Saharm123456@gmail.com>
2021-12-27 17:57:47 +0200

    project 7 fixes

---
+++ js/utilits.js
@@ -0,0 +1,66 @@
+const overlayList = document.querySelectorAll(".overlay")
+const closeButtons = document.querySelectorAll(".overlay__btn-close")
+const keys = { Escape: 27 }
+
+
+
+init()
+
+function init() {
+    setEventListeners()
+}
+
+
+function setEventListeners() {
+    overlayList.forEach(elm => elm.addEventListener("click", handleClickOverlay))
+    closeButtons.forEach(elm => elm.addEventListener("click", closePopup))
+
+
+    const popups = document.querySelectorAll('.popup')
+
+    popups.forEach((popup) => {
+        popup.addEventListener('click', (evt) => {
+            if (evt.target.classList.contains('popup_opened')) {
+                closePopup(popup)
+            }
+            if (evt.target.classList.contains('popup__close')) {
+                closePopup(popup)
+            }
+        })
+    })
+
+}
+
+// Overlay
+function handleClickOverlay(evt) {
+    const elm = evt.target
+        //Make sure the parent("overlay") element is clicked
+    if (elm.classList.contains("overlay_visible")) {
+        closePopup()
+    }
+}
+
+function handleKeyUp(evt) {
+    switch (evt.keyCode) {
+        case keys.Escape:
+            closePopup()
+            break;
+    }
+
+}
+
+//exported function
+export function openPopup(popup) {
+
+    popup.classList.add("overlay_visible")
+    document.addEventListener("keyup", handleKeyUp);
+}
+
+export function closePopup() {
+    const overlayVisible = document.querySelector(".overlay_visible")
+    if (overlayVisible !== null) {
+        overlayVisible.classList.remove("overlay_visible")
+        document.removeEventListener("keyup", handleKeyUp);
+    }
+
+}
