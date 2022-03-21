25c932067e225e067cb790d871dfeeec30605143
Sahar321 <Saharm123456@gmail.com>
2022-03-21 12:51:48 +0200

    Delete js directory

--- js/ConfigData.js
+++
@@ -1,7 +0,0 @@
-export const formConfigIndex = {
-    inputSelector: ".popup__input",
-    submitButtonSelector: ".popup__btn-submit",
-    inactiveButtonClass: "popup__btn-submit_inactive",
-    inputErrorClass: "popup__input_type_error",
-    errorClass: "popup__error_visible"
-};
